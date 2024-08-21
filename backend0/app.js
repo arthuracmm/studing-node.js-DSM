3a1c10d36e62f90b1301cab66ca0f9705d57e9d7
arthuracm <arthurcesarmarcelino@gmail.com>
2024-08-20 21:41:39 -0300

    node e express básico

---
+++ backend0/app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+const port = 3000;
+
+
+app.listen(port, () => {
+    console.log(`app rodando na porta ${port}`);
+})
+//rota do projeto
+app.get('/', (req, res) => {
+    console.log('rota raiz encontrada!');
+    res.send('<h3> Rota raiz encontrada </h3>')
+})
+
+app.get('/aluno', (req, res) => {
+    res.status(500).json({
+        status: 'sucess',
+        data: {
+            alunos: [
+                {
+                    id: 1,
+                    nome: 'Fulano',
+                    idade: 27
+                },
+                {
+                    id: 2,
+                    nome: 'Ciclano',
+                    idade: 32
+                }
+            ]
+        }
+        
+    });
+});
+
+app.get('*', (req, res) => {
+    res.send('<h3> Essa rota não existe </h3>')
+})
